a1f2d25f08d6686b95ce4a083e205f4c04941903
sendo <135673332+ttsendo@users.noreply.github.com>
2023-12-05 16:26:51 -0500

    Update funciones.js

--- js/funciones.js
+++ js/funciones.js
@@ -1,5 +1,5 @@
 //const url = 'https://api-2670689.onrender.com/usuario'
-const url = 'https://backendventas.onrender.com'
+const url = 'https://backendventas.onrender.com/venta'
 
 const listarVentas = async() => {
     //Objeto del html donde se deslegará la información
